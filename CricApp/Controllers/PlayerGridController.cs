16b3e8645f8737fc297377f8986f17eaf8945b22
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-12-28 19:05:08 +0000

    Added more test players

--- CricApp/Controllers/PlayerGridController.cs
+++ CricApp/Controllers/PlayerGridController.cs
@@ -10,17 +10,44 @@ namespace CricApp.Controllers
     {
         public ActionResult Players_Read([DataSourceRequest]DataSourceRequest request)
         {
-            var result = new List<PlayerViewModel>();
+            var result = new List<PlayerViewModel>(); //308967
 
             var testPlayer = new PlayerViewModel
             {
                 Pid = 35320,
-                FullName = "Sachin Ramesh Tendulkar",
+                Name = "Sachin Tendulkar",
                 Country = "India",
                 PlayingRole = "Top-order batsman"
             };
 
+            var testPlayer2 = new PlayerViewModel
+            {
+                Pid = 308967,
+                Name = "Jos Buttler",
+                Country = "England",
+                PlayingRole = "Wicketkeeper batsman"
+            };
+
+            var testPlayer3 = new PlayerViewModel
+            {
+                Pid = 303669,
+                Name = "Joe Root",
+                Country = "England",
+                PlayingRole = "Top-order batsman"
+            };
+
+            var testPlayer4 = new PlayerViewModel
+            {
+                Pid = 8608,
+                Name = "Jimmy Anderson",
+                Country = "England",
+                PlayingRole = "Bowler"
+            };
+
             result.Add(testPlayer);
+            result.Add(testPlayer2);
+            result.Add(testPlayer3);
+            result.Add(testPlayer4);
 
             var dsResult = result.ToDataSourceResult(request);
             return Json(dsResult);
--- CricApp/Models/PlayerViewModel.cs
+++ CricApp/Models/PlayerViewModel.cs
@@ -4,7 +4,7 @@
     {
         public long Pid { get; set; }
 
-        public string FullName { get; set; }
+        public string Name { get; set; }
 
         public string Country { get; set; }
 
